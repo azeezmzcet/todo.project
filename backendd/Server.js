9dc8b3e3aa15f9427a7d718a6c8921604a65add2
azeezmzcet <n.abdulazeez001@gmail.com>
2023-11-30 13:11:37 +0530

    add todo.project master

---
+++ backendd/Server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+
+
+const routes = require("./routes/ToDoRoutes");
+
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use(cors(
+  {
+    origin:["https://vercel.com/abdul-azeez-ns-projects"],
+    methods:["POST","GET"],
+    credentials:true
+  }
+));                   //
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB  successfully connected..."))
+  .catch((err) => console.log(err));
+
+app.use("/api", routes);
+
+app.listen(PORT, () => console.log(`Listening at ${PORT}...`));
+
+
